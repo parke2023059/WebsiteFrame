f318593461fc4881c9e39c96e20bdcc77ad81346
parke2023059 <90394656+parke2023059@users.noreply.github.com>
2022-02-16 09:17:15 -0500

    1
    base website

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const port = 5678
 const localName = '127.0.0.1'
+app.set('view engine', 'ejs')
+
 
 app.get('/', function(req,res){
   res.render('index.ejs')
